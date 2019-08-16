be83c334943851d474c3e899a3369315bd324959
celine lemenn <celinelemenn@gmail.com>
2019-08-16 12:45:41 +0200

    added images to the seed

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ gem 'turbolinks', '~> 5'
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass', '~> 5.6.1'
 gem 'simple_form'
+gem 'cloudinary', '~> 1.9.1'
+gem 'carrierwave', '~> 1.2'
 
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 puts "I am creating ingredients"
 
+Ingredient.delete_all
+
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
@@ -10,15 +12,41 @@ puts " we have now #{Ingredient.count} ingredients"
 
 puts "I am now creating cocktail"
 
-Cocktail.create(name: "Mochito")
-Cocktail.create(name: "Caipichina")
-Cocktail.create(name: "dark rise")
-Cocktail.create(name: "grog")
-Cocktail.create(name: "bad surprise")
+Cocktail.delete_all
+
+url1 ="www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2013/11/mojito-cocktails.jpg?itok=7ZS6egg_"
+url2 ="http://www.monstercocktails.com/image/279/645"
+url3 ="https://705628.smushcdn.com/1130679/wp-content/uploads/2019/05/20rustic-cocktail.jpg?lossy=1&strip=1&webp=1"
+url4 ="https://www.thespruceeats.com/thmb/VUIEfKs1iqGuO9FHqY4eYzPg6Ks=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/NavyGrogCocktail-565092779-577437e33df78cb62c3c37fd.jpg"
+url5 ="https://cocktailcollective.com.au/wp-content/uploads/2018/10/Surprise-Sour-565x565.jpg"
+
+cocktail1 = Cocktail.new(name: "Mochito")
+cocktail1.remote_photo_url = url1
+cocktail1.save
+
+cocktail2 = Cocktail.new(name: "Caipichina")
+cocktail2.remote_photo_url = url2
+cocktail2.save
+
+cocktail3 = Cocktail.new(name: "dark rise")
+cocktail3.remote_photo_url = url3
+cocktail3.save
+
+cocktail4 = Cocktail.new(name: "grog")
+cocktail4.remote_photo_url = url4
+cocktail4.save
+
+cocktail5 = Cocktail.new(name: "bad surprise")
+cocktail5.remote_photo_url = url5
+cocktail5.save
+
 
 puts "we have now #{Cocktail.count} cocktail"
 
 puts "I am now associating doses for each cocktail"
+
+Dose.delete_all
+
 Dose.create(description: "6 cl", cocktail_id: 1, ingredient_id: 1)
 Dose.create(description: "10 cl", cocktail_id: 1, ingredient_id: 2)
 Dose.create(description: "3 cl", cocktail_id: 2, ingredient_id: 1)
