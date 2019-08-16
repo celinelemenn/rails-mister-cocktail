415d2438043b68bf9cd5ff886085a3630b462e8d
celine lemenn <celinelemenn@gmail.com>
2019-08-16 12:02:17 +0200

    Add dotenv - Protect my secret data in .env file

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ Cocktail.create(name: "dark rise")
 Cocktail.create(name: "grog")
 Cocktail.create(name: "bad surprise")
 
-puts "we have now #{Cocktail.all} cocktail"
+puts "we have now #{Cocktail.count} cocktail"
 
 puts "I am now associating doses for each cocktail"
 Dose.create(description: "6 cl", cocktail_id: 1, ingredient_id: 1)
@@ -28,4 +28,4 @@ Dose.create(description: "5 cl", cocktail_id: 3, ingredient_id: 1)
 Dose.create(description: "a bit", cocktail_id: 4, ingredient_id: 3)
 Dose.create(description: "5 cl", cocktail_id: 4, ingredient_id: 4)
 
-puts "I have now created #{Dose.all}"
+puts "I have now created #{Dose.count}"
